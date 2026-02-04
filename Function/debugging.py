a690b3178ec920fea1b1cedc224a7824675c2535
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-02-04 11:29:15 +0530

    k

--- Function/debugging.py
+++ Function/debugging.py
@@ -7,3 +7,8 @@ def multiply(*numbers):
 
 print("start")
 print(multiply(1, 2, 3))
+
+# step by step debugging
+# f5 for start debugging
+# f10 for continue debugging to the next line and close debuging
+# f11 for one by one line debugging
