353c2fa8ce31447fd441570da6ac058d8a82434a
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-02-05 10:04:26 +0530

    k

---
+++ Data Structure/unpackingoperator.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3]
+print(*numbers)
+print(1, 2, 3)
+
+values = list(range(5))
+values = [*range(5), *"Hello"]
+print(values)
+
+first = {"x": 1}
+second = {"x": 10, "y": 2}
+combined = {**first, **second, "z": 1}
+print(combined)
