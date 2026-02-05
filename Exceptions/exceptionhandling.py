750e6ecb2cf5faf26bcf6dba690bb1d7e43bd2eb
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-02-05 12:09:21 +0530

    k

--- Exceptions/exceptionhandling.py
+++ Exceptions/exceptionhandling.py
@@ -7,3 +7,12 @@ except ValueError as ex:
 else:
     print("No exception were thrown. ")
 print("Exceution continues.")
+
+
+# a = 10
+# b = 0
+
+# try:
+#     print(a/b)
+# except ZeroDivisionError:
+#     print("Cannot divided bt Zero. ")
