e1e3d769b7a535f94c9a9c74c994b5384cefbf73
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-02-06 16:29:01 +0530

    k

---
+++ Modules/app.py
@@ -0,0 +1,18 @@
+# from sales import calc_shipping, calc_tax
+# import sales
+
+# sales.calc_shipping()
+
+# calc_shipping()
+# calc_tax
+
+
+# from ecommerce.sales import calc_tax, calc_shipping
+# from ecommerce import sales
+
+from ecommerce.shopping import sales
+
+# print(dir(sales))
+# print(sales.__name__)
+# print(sales.__package__)
+# print(sales.__file__)
