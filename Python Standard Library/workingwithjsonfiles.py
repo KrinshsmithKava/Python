4c98c3b6df0462dc11ffce3c56ff378fe0535190
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-02-09 11:44:57 +0530

    k'

--- Python Standard Library/workingwithjsonfiles.py
+++ Python Standard Library/workingwithjsonfiles.py
@@ -2,13 +2,13 @@ import json
 from pathlib import Path
 
 
-# movies = [
-#     {"id": 1, "title": "Terminator", "year": 1989},
-#     {"id": 1, "title": "Kindergarten Cop", "year": 1989}
-# ]
+movies = [
+    {"id": 1, "title": "Terminator", "year": 1989},
+    {"id": 2, "title": "Kindergarten Cop", "year": 1989}
+]
 
-# data = json.dumps(movies)
-# Path("movies.json").write_text(data)
+data = json.dumps(movies)
+Path("movies.json").write_text(data)
 
 data = Path("movies.json").read_text()
 movies = json.loads(data)
