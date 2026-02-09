6d2b084fe016ac35bf45d1b90f16a8dcc75c42f2
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-02-09 11:46:14 +0530

    k

--- Python Standard Library/workingwithjsonfiles.py
+++ Python Standard Library/workingwithjsonfiles.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 movies = [
     {"id": 1, "title": "Terminator", "year": 1989},
-    {"id": 2, "title": "Kindergarten Cop", "year": 1989}
+    {"id": 2, "title": "Kindergarten Cop", "year": 1993}
 ]
 
 data = json.dumps(movies)
