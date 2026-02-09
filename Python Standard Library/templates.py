fa17ed57bebc58154f90170ea58d4ecb96a7000b
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-02-09 16:31:33 +0530

    k

---
+++ Python Standard Library/templates.py
@@ -0,0 +1,23 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from pathlib import Path
+from string import Template
+import smtplib
+
+template = Template(Path("template.html").read_text())
+
+message = MIMEMultipart()
+message["from"] = "Krinshsmith Kava"
+message["to"] = "krinshkava2407@gmail.com"
+message["subject"] = "This is a test"
+body = template.substitute({"name": "Jhon"})
+message.attach(MIMEText(body, "html"))
+message.attach(MIMEImage(Path("D:\python.png").read_bytes()))
+
+with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login("krinshsmith.nivzen@gmail.com", "ghed cezv qkns gjli")
+    smtp.send_message(message)
+    print("Sent...")
