db1d05c5484da1496e2bb2c954798716fb3ac56e
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-02-09 16:03:53 +0530

    k

--- Python Standard Library/sendingemails.py
+++ Python Standard Library/sendingemails.py
@@ -3,14 +3,14 @@ from email.mime.text import MIMEText
 import smtplib
 
 message = MIMEMultipart()
-message["from"] = "Mosh Hamedani"
-message["to"] = "testuser@codewithmosh.com"
+message["from"] = "Krinshsmith Kava"
+message["to"] = "krinshkava2407@gmail.com"
 message["subject"] = "This is a test"
 message.attach(MIMEText("Body"))
 
 with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login("testuser@codewithmosh.com", "todayskyisblue1234")
+    smtp.login("krinshsmith.nivzen@gmail.com", "ghed cezv qkns gjli")
     smtp.send_message(message)
     print("Sent...")
