a5a3152a2f86a3fe09ead2e21d0ced1e3da449f5
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-02-11 18:09:24 +0530

    k

---
+++ Popular Python Packages/workingwithexcelfiles.py
@@ -0,0 +1,29 @@
+import openpyxl
+
+#wb = openpyxl.workbook()
+wb = openpyxl.load_workbook("transactions.xlsx")
+print(wb.sheetnames)
+
+sheet = wb["Sheet1"]
+
+cell = sheet["a1"]
+column = sheet["a"]
+cells = sheet["a:c"]
+sheet["a1:c3"]
+sheet[1:3]
+
+sheet.append([1, 2, 3])
+wb.save("transaction2.xlsx")
+# print(cells)
+# print(cell.value)
+# print(cell.row)
+# print(cell.column)
+# print(cell.coordinate)
+
+# print(sheet.max_row)
+# print(sheet.max_column)
+
+# for row in range(1, sheet.max_row + 1):
+#     for column in range(1, sheet.max_column + 1):
+#         cell = sheet.cell(row, column)
+#         print(cell.value)
