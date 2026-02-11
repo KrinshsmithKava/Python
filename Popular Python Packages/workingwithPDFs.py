7df7e17ef2a4fc0f07cbe6ac58f95a3b6eff1d29
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-02-11 17:15:02 +0530

    k

--- Popular Python Packages/workingwithPDFs.py
+++ Popular Python Packages/workingwithPDFs.py
@@ -1,11 +1,17 @@
 import PyPDF2
 
-with open("first.pdf", "rb")as file:
-    reader = PyPDF2.PdfReader(file)
-    print(len(reader.pages))
-    page = reader.pages[0]
-    page.rotate(90)
-    writer = PyPDF2.PdfWriter()
-    writer.add_page(page)
-with open("rotated.pdf", "wb") as output:
-        writer.write(output)
+# with open("first.pdf", "rb")as file:
+#     reader = PyPDF2.PdfReader(file)
+#     print(len(reader.pages))
+#     page = reader.pages[0]
+#     page.rotate(90)
+#     writer = PyPDF2.PdfWriter()
+#     writer.add_page(page)
+# with open("rotated.pdf", "wb") as output:
+#         writer.write(output)
+
+merger = PyPDF2.PdfMerger()
+file_names = ["first.pdf", "second.pdf"]
+for file_name in file_names:
+    merger.append(file_name)
+merger.write("combined.pdf")
