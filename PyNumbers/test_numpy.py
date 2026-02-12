67596d11b7000faae9cc50b8cb6baf6c06390597
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-02-12 12:10:48 +0530

    k'

---
+++ PyNumbers/test_numpy.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+# array = np.array([ [1, 2, 3], [4, 5, 6] ])
+# print(array)
+# print(type(array))
+# print(array.shape)
+
+# array = np.zeros((3, 4), dtype=int)
+# array = np.ones((3, 4), dtype=int)
+# array = np.full((3, 4), 5, dtype=int)
+# array = np.random.random((3, 4))
+# print(array)
+# print(array[0, 0])
+# print(array > 0.2)
+# print(array[array > 0.2])
+# print(np.sum(array))
+# print(np.floor(array))
+# print(np.round(array))
+
+# Arithmetic Operations
+
+first = np.array([1, 2, 3])
+second = np.array([1, 2, 3])
+print(first + second)
+print(first - second)
+print(first * second)
+print(first / second)
+print(first % second)
+
+print(first + 2)
+print(first - 2)
+print(first * 2)
+print(first / 2)
+print(first % 2)
+
+#dimensons for inch and cm 
+
+# dimensions_inch = np.array([1, 2, 3])
+# dimensions_cm = dimensions_inch * 2.54
+# print(dimensions_cm)
+
+# dimensions_inch = [1, 2, 3]
+# dimensions_cm = [x * 2.54 for x in dimensions_inch]
